39d78bfcecf8feaab72d4ef393339335f67a0cc9
Namish <namish@utexas.edu>
2022-10-19 17:57:17 -0500

    Most updated version. Basic functionality is same.

--- data.py
+++ data.py
@@ -2,11 +2,12 @@ import pymongo
 
 champions = {}
 client = pymongo.MongoClient("mongodb://localhost:27017/")
-rift = client.LeagueData.SummonersRift
+rift = client.LeagueData.challenger
 team = 0
 bigdcounter = 0
 for match in rift.find({}):
-    win = True if match['info']['teams'][0]['win'] == True else False
+    win1 = True if match['info']['teams'][0]['win'] == True else False
+    win2 = not win1
     for i in range(0, 10):
         if i == 0:
             print("Team 1:")
@@ -14,19 +15,17 @@ for match in rift.find({}):
         elif i == 5:
             print("\nTeam 2")
             team = 1
-            if not win: 
-                win = True
         champ = match['info']['participants'][i]['championName']
         if champ == "Gangplank" and match['info']['participants'][i]['summonerName'] == "DankDanzo":
             bigdcounter += 1
 
         if not (champ in champions):
-            if win:
+            if ((i < 5 and win1) or (i >= 5 and win2)):
                 champions[champ] = [1, 1]
             else: 
                 champions[champ] = [0, 1]
         else: 
-            if win:
+            if (i < 5 and win1) or (i >= 5 and win2):
                 champions[champ][0] += 1
             champions[champ][1] += 1
 
@@ -35,10 +34,12 @@ for match in rift.find({}):
     print("\n")
 
 winrates = {}
+champions = dict(sorted(champions.items(), key=lambda item: item[1], reverse = True)) #sort by games played
 for champ in champions: 
     winrates[champ] = champions[champ][0]/champions[champ][1]
 
-winrates = dict(sorted(winrates.items(), key=lambda item: item[1], reverse=True))
+print(winrates)
+#winrates = dict(sorted(winrates.items(), key=lambda item: item[1], reverse=True)) #sort by winrate
 
 for champ in winrates: 
     print(f"{champ}: {round(winrates[champ]*100, 2)}% in {champions[champ][1]} games")
--- fetch.py
+++ fetch.py
@@ -5,12 +5,21 @@ def requestSummonerData(region, summonerName, APIKey):
     response = requests.get(URL)
     return response.json()
 
+def requestSummonerDataByID(region, summonerID, APIKey):
+    URL = "https://" + region + ".api.riotgames.com/lol/summoner/v4/summoners/" + summonerID + "?api_key=" + APIKey
+    response = requests.get(URL)
+    return response.json()
+
 def getMatchHistory(region, puuid, APIKey):
     URL = "https://" + region + ".api.riotgames.com/lol/match/v5/matches/by-puuid/" + puuid + "/ids" + "?api_key=" + APIKey + "&count=100"
     return requests.get(URL).json()
     
 def getMatch(region, matchID, APIKey):
     URL = "https://" + region + ".api.riotgames.com/lol/match/v5/matches/" + matchID + "?api_key=" + APIKey
+    return requests.get(URL).json()
+
+def getChallengerQueue(region, APIKey):
+    URL = "https://" + region + ".api.riotgames.com/lol/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5?api_key=" + APIKey
     return requests.get(URL).json()
 
 def main():
@@ -20,31 +29,48 @@ def main():
     # APIKey = input('Enter your API Key: ')
     region = "NA1"
     summonerName = "morivilance"
-    APIKey = ""
+    APIKey = "RGAPI-60b4d840-5938-48be-b7a6-ea473140b4ec"
     responseJSON = requestSummonerData(region, summonerName, APIKey)
     client = pymongo.MongoClient("mongodb://localhost:27017/")
     aram = client.LeagueData.ARAM
     ranked = client.LeagueData.Ranked
     rift = client.LeagueData.SummonersRift
-
+    challenger = client.LeagueData.challenger
     # print("\nId: ", responseJSON['id'])
     # print("Account Id: ", responseJSON['accountId'])
-    print("PUUID: ", responseJSON['puuid'])
+    # print("PUUID: ", responseJSON['puuid'])
     # print("Profile Icon Id: ", responseJSON['profileIconId'])
     # print("Revision Date: ", responseJSON['revisionDate'])
-    print("Name: ", responseJSON['name'])
-    print("Summoner Level : ", responseJSON['summonerLevel'])
+    # print("Name: ", responseJSON['name'])
+    # print("Summoner Level : ", responseJSON['summonerLevel'])
 
-    matchHistory = getMatchHistory("AMERICAS", responseJSON['puuid'], APIKey)
+    cq = getChallengerQueue(region, APIKey)
     count = 0
-    for i in range(0, 99):
-        match = getMatch("AMERICAS", matchHistory[i], APIKey)
-        
-        if match['info']['gameMode'] == 'CLASSIC':
-            rift.insert_one(match)
-            count += 1
 
+    #get non duplicate challenger games
+    entries = cq['entries']
+    currentProgress = 20
+    for i in range(0, currentProgress):
+        entries.pop(i)
+
+    for player in entries:
+        summonerName = player['summonerId']
+        responseJSON = requestSummonerDataByID(region, summonerName, APIKey)
+        matchHistory = getMatchHistory("AMERICAS", responseJSON['puuid'], APIKey)
+        print(len(matchHistory))
+        for i in range(0, 99):
+            print(f"{i} {matchHistory[i]}")
+            match = getMatch("AMERICAS", matchHistory[i], APIKey)
+            cursor = challenger.find({ "_id" : matchHistory[i]})
+            point = next(cursor, None)
+            print(point)
+            print(match['info']['queueId'])
+            if match['info']['queueId'] == 420 and point == None:
+                print("inserting")
+                challenger.insert_one({"_id": matchHistory[i], "metadata": match['metadata'], "info": match['info']})
+                count += 1
     print(count)
+
 
 
 
